2b1a0113fd0ba64609c43b8a9a659b830673491a
Sona9305 <sonwabiledubeni93gmail.com>
2023-02-17 12:32:35 -0800

    printing digits and alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar(10); /* this is an ascii code for new line*/
+	return (0);
+}
