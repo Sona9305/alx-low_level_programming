e702278a4aaeac4e9419dd4b8ef5d64d4252315b
Sona9305 <sonwabiledubeni93gmail.com>
2023-02-21 09:10:55 -0800

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
-
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_alphabet - in lower case
@@ -13,5 +12,5 @@ void print_alphabet(void)
 	{
 		putchar(i);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
