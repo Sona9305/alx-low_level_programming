273a014904702142186dd5eb86c4e8b7e18bd315
Sona9305 <sonwabiledubeni93gmail.com>
2023-02-21 03:24:36 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,18 +6,16 @@
  */
 
 int main(void) 
-{
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
+ {
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
-return (0);
-}
-
-
+	return (0);
+ }
