ff2b92e883c4b8f8ecaee001883545e53b28352f
Sona9305 <sonwabiledubeni93gmail.com>
2023-02-22 02:40:19 -0800

    to print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        putchar("c");
+    }
+    putchar("\n");
+    return (0);
+}
