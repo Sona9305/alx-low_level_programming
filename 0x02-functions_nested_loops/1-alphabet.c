bf945c9c3c0dda27960daf363b9d70a9986fc4c9
Sona9305 <sonwabiledubeni93gmail.com>
2023-02-22 02:14:39 -0800

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -9,9 +9,9 @@ int main(void)
 {
 	char i;
 
-	for (i = "a"; i < "z"; i++)
+	for (i="a"; i<"z"; i++)
 	{
-		putchar(i);
+		putchar("%c", i);
 	}
 	putchar("\n");
 	return (0);
