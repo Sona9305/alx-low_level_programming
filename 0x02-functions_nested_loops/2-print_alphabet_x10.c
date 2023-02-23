20b184d2515c61064506e1666ae5bf8714c16f99
Sona9305 <sonwabiledubeni93gmail.com>
2023-02-22 17:32:47 -0800

    print charactor 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,41 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet 10 times
- * Return:void
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
-
 {
-
-	char c;
-
-	int i = 0;
-
-
-
-	while (i < 10)
-
-	{
-
-		c = 'a';
-
-		while (c <= 'z')
-
-		{
-
-			putchar(c);
-
-			c++;
-
-		}
-
-		putchar('\n');
-
-		i++;
-
-	}
-
+    int i = 0;
+    while (i < 10)
+    {
+        _putchar("c");
+        i++;
+    }
+    _putchar("\n");
 }
+
